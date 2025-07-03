7303d912bfc915c1fbeb528597f03420afece1aa
manish-gkv <manishkumarslmp@gmail.com>
2025-07-03 14:34:40 +0530

    tag change to h3

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,12 +12,12 @@ export default function Hero() {
         <>
             <div id="home" className="flex flex-col h-screen px-8 items-center justify-center gap-2 sm:gap-6 font-medium w-full">
                 <div className=""><span className="bg-gray-200 rounded-full text-sm py-1 px-3">FullStack Developer</span></div>
-                <div className="text-black font-bold text-4xl md:text-6xl text-center tracking-tighter text-balance">
+                <h2 className="text-black font-bold text-4xl md:text-6xl text-center tracking-tighter text-balance">
                     Building Scalable Web Solutions from Frontend to Backend
-                </div>
-                <div className="text-lg text-gray-500 text-center text-balance">
+                </h2>
+                <h2 className="text-lg text-gray-500 text-center text-balance">
                     I create modern, responsive, and high-performance web applications using technologies like NextJs, ReactJs, ExpressJs and NodeJs.
-                </div>
+                </h2>
                 <div className="flex flex-wrap sm:flex-row gap-4">
                     <div className="bg-black text-white text-center px-6 py-2 rounded text-nowrap w-full md:w-fit cursor-pointer" onClick={()=>handleClick('projects')}>
                         View My Work
