08273524dd15209f6abf7be9a681dbcc50d76667
manish-gkv <manishkumarslmp@gmail.com>
2025-08-17 03:32:03 +0530

    updated resume link

--- src/utility/constant.js
+++ src/utility/constant.js
@@ -6,6 +6,6 @@ export const MENU_LIST = [
     { id: 'contact', label: 'Contact' },
 ];
 
-export const RESUME_LINK = 'https://drive.google.com/file/d/1-ljK5Q8XJ4RrZaNEdyHbDXomaPIquLXf/view?usp=sharing';
+export const RESUME_LINK = 'https://drive.google.com/file/d/1n7ypJZOy-rAlWt6zcNhQjV-XTE9_3Htb/view?usp=sharing';
 
 export const EMAIL_ACCESS_KEY = "c2c37154-b41b-4874-8495-79e79220fcad"
